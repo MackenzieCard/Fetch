06d760361afa7b1858a2871aa6455174be153ab9
Mackenzie Card <kenziecard@gmail.com>
2022-09-28 14:11:21 -0400

    created users and added to database with batch import

---
+++ server/batchImport.js
@@ -0,0 +1,25 @@
+const {users} = require("./data"); 
+
+const { MongoClient } = require("mongodb"); 
+require("dotenv").config(); 
+const { MONGO_URI } = process.env; 
+
+const options = {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+}; 
+
+const batchImport = async () => {
+    const client = new MongoClient(MONGO_URI, options); 
+    try {
+        await client.connect(); 
+        const db = client.db("Fetch_Database");
+        await db.collection("users").insertMany(users); 
+        console.log("success!"); 
+    } catch (error) {
+        console.log(error); 
+    } finally {
+        client.close(); 
+    }
+}; 
+batchImport(); 
---
+++ server/data.js
@@ -0,0 +1,83 @@
+const users = [
+    {
+        id: "1001",
+        ownerName: "Maggie",
+        dogName: "Bruno", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC MAGGIE.jpg",
+        location: "Quebec City", 
+        joined:"July 18, 2019"
+    }, 
+    {
+        id: "1002",
+        ownerName: "Gina",
+        dogName: "Frankie", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC GINA.jpg",
+        location: "Laval", 
+        joined:"January 21, 2021"
+    }, 
+    {
+        id: "1003",
+        ownerName: "Taylor",
+        dogName: "Dixie", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC TAYLOR.jpg",
+        location: "Quebec City", 
+        joined:"February 26, 2022"
+    }, 
+    {
+        id: "1004",
+        ownerName: "Marc",
+        dogName: "Oreo", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC MARC.jpg",
+        location: "Montréal", 
+        joined:"January 20, 2019"
+    },
+    {
+        id: "1005",
+        ownerName: "Casey",
+        dogName: "Harley", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC CASEY.jpg",
+        location: "Dorval", 
+        joined:"April 26, 2021"
+    },
+    {
+        id: "1006",
+        ownerName: "Drew",
+        dogName: "Finn", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC DREW.jpg",
+        location: "Montréal", 
+        joined:"April 26, 2021"
+    },
+    {
+        id: "1007",
+        ownerName: "Jason",
+        dogName: "Coco", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC JASON.jpg",
+        location: "Montréal", 
+        joined:"January 27, 2019"
+    },
+    {
+        id: "1008",
+        ownerName: "Derek",
+        dogName: "Boston", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC DEREK.jpg",
+        location: "Montréal", 
+        joined:"July 17, 2021"
+    },
+    {
+        id: "1009",
+        ownerName: "Nicole",
+        dogName: "Zeus", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC NICOLE.jpg",
+        location: "Montréal", 
+        joined:"October 14, 2018"
+    },
+    {
+        id: "1010",
+        ownerName: "Florence",
+        dogName: "Minnie", 
+        avatarSrc: "/Images/profile-pics/PROJECT PIC FLORENCE.jpg",
+        location: "Montréal", 
+        joined:"January 11, 2022"
+    },
+] 
+module.exports = {users}
